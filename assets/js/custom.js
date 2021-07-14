5b584658d98aa726440b7b36330505cf598b96d9
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-07-14 16:54:03 +0600

    artisti commit

---
+++ assets/js/custom.js
@@ -0,0 +1,67 @@
+
+
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 6,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+
+});
+
+
+$('.carousel').carousel({
+    interval: 3000
+})
+
+
+
+
+    $(document).ready(function() {
+     
+      $("#ltst").ltstt({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,4],
+          itemsDesktopSmall : [979,4]
+     
+      });
+     
+    });
+
+
+/*wow min js script*/
+
+(function ($) {
+    
+    // Init Wow
+    wow = new WOW( {
+        animateClass: 'animated',
+        offset:       100
+    });
+    wow.init();
+    
+    
+    
+
+    
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
